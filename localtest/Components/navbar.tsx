83115f8b99bf4744d6e6f58be945700562371a40
dlt-lilian <dulotlilian@gmail.com>
2025-12-26 18:10:13 +0100

    Test de daisyui et debut navbar"

--- localtest/Components/navbar.tsx
+++ localtest/Components/navbar.tsx
@@ -1,7 +1,37 @@
+
 export default function Navbar() {
     return (
-        <>
-            <p>Navbar</p>
-        </>
+        <nav class="grid grid-cols-3 gap-5 px-32 py-5 shadow-xl">
+            <img
+                src="/logo.png"
+                alt="Logo"
+            />
+
+            <div className="flex justify-center space-x-5">
+                <div>
+                    <div className="dropdown dropdown-center">
+                        <div tabIndex="0" role="button" className="">Vitrines</div>
+                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                            <li><a>Item 1</a></li>
+                            <li><a>Item 2</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="dropdown dropdown-center">
+                        <div tabIndex="0" role="button" className="">Meules</div>
+                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                            <li><a>Item 1</a></li>
+                            <li><a>Item 2</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex justify-end">
+                <p>Panier user etc...</p>
+            </div>
+        </nav>
     );
 }
