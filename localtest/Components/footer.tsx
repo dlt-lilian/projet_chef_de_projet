c24c348ec1d27cb105a09b7dd419e72b496ada96
dlt-lilian <dulotlilian@gmail.com>
2025-12-26 17:51:54 +0100

    Add Daisyui and configure layout

---
+++ localtest/Components/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <p>Je suis le Footer</p>
+    )
+}
---
+++ localtest/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+        <>
+            <p>Je suis la page d’accueil</p>
+
+            <button class="btn btn-primary">Primary</button>
+        </>
+
+    )
+}
