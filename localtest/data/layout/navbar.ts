dd05801f6f30421f48460a764fd5e8cc39be4d2d
dlt-lilian <dulotlilian@gmail.com>
2025-12-29 17:32:26 +0100

    MAJ json pour la navbar

--- localtest/data/layout/navbar.ts
+++ localtest/data/layout/navbar.ts
@@ -6,15 +6,155 @@ export const navigation = [
             title: "Parcourir les catégories",
             link: "/productgrid",
             content: [
-                { label: "Objets en vitrine", href: "/objets-en-vitrine" },
-                { label: "Art de table", href: "/art-de-table" },
-                { label: "Bijou", href: "/bijou" },
-                { label: "Musique", href: "/musique" },
-                { label: "Mode", href: "/mode" },
-                { label: "Mobilier", href: "/mobilier" },
-                { label: "Chambre", href: "/chambre" },
-                { label: "Meubles de rangements", href: "/meubles-de-rangements" },
-                { label: "Tables", href: "/tables" }
+                {
+                    label: "Objets décoratifs & de collection",
+                    href: "/objets-en-vitrine",
+                    items: [
+                        "Art Déco",
+                        "Bistrot",
+                        "Céramique",
+                        "Horlogerie",
+                        "Industriel",
+                        "Maritime",
+                        "Objets anciens",
+                        "Objets de collection",
+                        "Objets ethniques",
+                        "Religieux",
+                        "Vases"
+                    ]
+                },
+                {
+                    label: "Loisirs & divertissement",
+                    href: "/objets-en-vitrine",
+                    items: [
+                        "Jouets & jeux",
+                        "Multimédia",
+                        "Vinyles",
+                        "Instruments de musique"
+                    ]
+                },
+                {
+                    label: "Militaria & plein air",
+                    href: "/objets-en-vitrine",
+                    items: ["Militaria", "Chasse", "Pêche"]
+                },
+                {
+                    label: "Art de la table & cuisine",
+                    href: "/objets-en-vitrine",
+                    items: [
+                        "Argenterie",
+                        "Assiettes",
+                        "Bols & coupelles",
+                        "Bougeoirs & chandeliers",
+                        "Bouteilles, carafes & pichets",
+                        "Coupes",
+                        "Couverts",
+                        "Cuisine",
+                        "Cuivre",
+                        "Plats, saladiers & soupières",
+                        "Plateaux & dessous de plat",
+                        "Porcelaine",
+                        "Porte-couteaux",
+                        "Seaux à glace",
+                        "Services",
+                        "Théières, cafetières & bouilloires",
+                        "Verres"
+                    ]
+                },
+                {
+                    label: "Bijoux & accessoires",
+                    href: "/objets-en-vitrine",
+                    items: [
+                        "Bagues",
+                        "Boucles d’oreilles",
+                        "Bracelets",
+                        "Broches",
+                        "Colliers & chaînes",
+                        "Montres",
+                        "Lunettes"
+                    ]
+                },
+                {
+                    label: "Mode & textile",
+                    href: "/objets-en-vitrine",
+                    items: ["Maroquinerie", "Textile"]
+                },
+                {
+                    label: "Mobilier",
+                    href: "/objets-en-vitrine",
+                    items: [
+                        {
+                            label: "Mobilier de rangement",
+                            items: [
+                                "Armoires",
+                                "Bars & comptoirs",
+                                "Bibliothèques",
+                                "Bonnetières",
+                                "Buffets & bahuts",
+                                "Chevets",
+                                "Chiffonniers",
+                                "Classeurs & vestiaires",
+                                "Coffres, malles & caisses",
+                                "Commodes",
+                                "Confituriers",
+                                "Encoignures",
+                                "Enfilades",
+                                "Étagères",
+                                "Living",
+                                "Meubles d’appoint",
+                                "Porte-revues",
+                                "Secrétaires",
+                                "Vaisseliers",
+                                "Vitrines"
+                            ]
+                        },
+                        {
+                            label: "Tables",
+                            items: [
+                                "Consoles",
+                                "Dessertes",
+                                "Guéridons",
+                                "Sellettes",
+                                "Tables à manger",
+                                "Tables basses",
+                                "Tables de jardin",
+                                "Tables gateleg",
+                                "Tables gigognes"
+                            ]
+                        },
+                        {
+                            label: "S’asseoir",
+                            items: [
+                                "Bancs",
+                                "Banquettes",
+                                "Canapés",
+                                "Chaises",
+                                "Fauteuils",
+                                "Fauteuils à bascule",
+                                "Méridiennes",
+                                "Repose-pieds",
+                                "Tabourets"
+                            ]
+                        },
+                        {
+                            label: "Mobilier spécialisé",
+                            items: [
+                                "Bureaux",
+                                "Coiffeuses",
+                                "Meubles de métier",
+                                "Meubles scandinaves",
+                                "Paravents",
+                                "Mobilier de salle de bain",
+                                "Salons"
+                            ]
+                        }
+                    ]
+                },
+                {
+                    label: "Chambre",
+                    href: "/objets-en-vitrine",
+                    items: ["Lits", "Matelas", "Sommiers"]
+                }
             ],
             pages: [
                 { label: "Le magasin", icon: "lucide:atom", href: "/magasin" },
@@ -22,13 +162,12 @@ export const navigation = [
                 { label: "Vendre", icon: "lucide:atom", href: "/vend" }
             ]
         },
-
         searchIcon: "lucide:search",
-
         rightContent: [
             { label: "Nous contacter", icon: "lucide:atom", href: "#" },
             { label: "Se connecter", icon: "lucide:user-round", href: "#" },
-            { label: "Panier", icon: "lucide:shopping-cart", href: "#" },
+            { label: "Panier", icon: "lucide:shopping-cart", href: "#" }
         ]
     }
 ];
+    
